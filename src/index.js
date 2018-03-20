d9bda9ac4bf641de3a557796278e77a6446b092b
Ian Hansson <ian.hansson@gmail.com>
2018-03-20 10:55:39 +0000

    Initial tests

--- src/index.js
+++ src/index.js
@@ -2,34 +2,34 @@
 
 const semiTone = 1.059463094359
 
-function getLetterDistanceFromA(letter){
+function getLetterDistanceFromA(letter) {
     const distances = {
-        'a' : 0,
-        'a#' : 1,
-        'b' : 2,
-        'c' : 3,
-        'c#' : 4,
-        'd' : 5,
-        'd#' : 6,
-        'e' : -5,
-        'f' : -4,
-        'f#' : -3,
-        'g' : -2,
-        'g#' : -1
+        'a': 0,
+        'a#': 1,
+        'b': 2,
+        'c': 3,
+        'c#': 4,
+        'd': 5,
+        'd#': 6,
+        'e': -5,
+        'f': -4,
+        'f#': -3,
+        'g': -2,
+        'g#': -1
     };
     return distances[letter]
 }
 
-function shiftFrequencyOctave(freq, octave){
+function shiftFrequencyOctave(freq, octave) {
     return freq * Math.pow(semiTone, octave * 12)
 }
 
-function calculateFrequencyFromLetter(letter){
+function calculateFrequencyFromLetter(letter) {
     const distance = getLetterDistanceFromA(letter.replace(/[0-9]/g, ''))
     let freq = 440 * Math.pow(semiTone, distance)
 
     const octave = letter.match(/\d+/)
-    if(octave){
+    if (octave) {
         freq = shiftFrequencyOctave(freq, octave - 4)
     }
     return freq
@@ -43,6 +43,7 @@ const TonePlayer = {
         defaultDelay: 0,
         defaultAttack: 0,
         defaultDecay: 0,
+        defaultDistortion: 0,
     },
     create: function(args) {
         const player = Object.create(this);
@@ -56,6 +57,7 @@ const TonePlayer = {
         this.defaultDuration = this.options.defaultDuration
         this.defaultAttack = this.options.defaultAttack
         this.defaultDecay = this.options.defaultDecay
+        this.defaultDistortion = this.options.defaultDistortion
         this.Context = this.options.context || createTonePlayerContext()
     },
     playNote: function(note, options) {
@@ -69,9 +71,10 @@ const TonePlayer = {
 
         const startTime = Context.currentTime + (options.delay || this.options.defaultDelay)
 
-
         const attack = options.attack || this.options.defaultAttack
-        const volume = options.volume || this.options.defaultVolume;
+        let volume = options.volume || this.options.defaultVolume
+
+        const distortion = options.distortion || this.options.defaultDistortion
 
         if (attack === 0) {
             gainNode.gain.setValueAtTime(volume, startTime);
@@ -80,13 +83,25 @@ const TonePlayer = {
             gainNode.gain.linearRampToValueAtTime(volume, startTime + attack);
         }
 
-        gainNode.connect(Context.destination)
+        let pulseCurve = new Float32Array(256);
+        for (var i = 0; i < 128; i++) {
+            pulseCurve[i] = -0.8;
+            pulseCurve[i + 128] = .8;
+        }
+        const waveShaperNode = Context.createWaveShaper();
+        // waveShaperNode.curve = pulseCurve;
+        waveShaperNode.curve = makeDistortionCurve(distortion);
+        waveShaperNode.oversample = '4x';
 
         const oscillator = Context.createOscillator();
         oscillator.type = options.type || this.options.defaultType;
         oscillator.frequency.setValueAtTime(calculateFrequencyFromLetter(note), startTime);
         oscillator.connect(gainNode)
         oscillator.start();
+
+        gainNode.connect(waveShaperNode)
+
+        waveShaperNode.connect(Context.destination)
 
         const duration = options.duration || this.options.defaultDuration;
         const endTime = startTime + duration;
@@ -98,11 +113,6 @@ const TonePlayer = {
             gainNode.gain.setValueAtTime(volume, endTime);
             gainNode.gain.linearRampToValueAtTime(0, endTime + decay);
             oscillator.stop(endTime + decay)
-        }
-
-        oscillator.onended = () => {
-            gainNode.disconnect(Context.destination)
-            oscillator.disconnect(gainNode)
         }
 
     },
@@ -133,6 +143,20 @@ const TonePlayer = {
     }
 }
 
+function makeDistortionCurve(amount) {
+    var k = typeof amount === 'number' ? amount : 50,
+        n_samples = 44100,
+        curve = new Float32Array(n_samples),
+        deg = Math.PI / 180,
+        i = 0,
+        x;
+    for (; i < n_samples; ++i) {
+        x = i * 2 / n_samples - 1;
+        curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
+    }
+    return curve;
+};
+
 const createTonePlayerContext = () => {
     return new(window.AudioContext || window.webkitAudioContext)();
 }
@@ -148,9 +172,10 @@ const keyboard = TonePlayer.create({
     context: context
 });
 
-keyboard.playChord(['a','c','e'],{
+keyboard.playChord(['a', 'c', 'e'], {
+    distortion: 25500
 });
 
-keyboard.playChord(['e','g','a'],{
+keyboard.playChord(['e', 'g', 'a'], {
     delay: 1
 });
